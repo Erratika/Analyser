6a90385e8f8cd07ea6c54fa42cbb1bfa421831a9
Marc Murray <mmarc.96@gmail.com>
2022-06-28 11:29:19 +0100

    Init

---
+++ src/main/java/LexicalAnalyser.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+
+public class LexicalAnalyser {
+
+    public int letterCount(){
+
+    }
+    public int wordCount(){}
+    public Map<Character,Integer> letterFrequency(){
+
+    }
+}
