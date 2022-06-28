da96a9218a4a2979f454f9e4215f67ae2665b969
M-Matson <108122866+M-Matson@users.noreply.github.com>
2022-06-28 13:53:10 +0100

    Update Main.java
    Scanner in main method, just for lettter count method, add more for other methods?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        LexicalAnalyser s = new LexicalAnalyser();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word or sentence ");
+        String words = scan.nextLine();
+
+        System.out.println("Your word or sentence has " + s.letterCount(words) + " Characters" );
 
     }
 }
